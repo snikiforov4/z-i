b19b1c052b421a87475a9c3b3e3fb2d73d1d12b6
Serhii Nykyforov <04mailbox@gmail.com>
2018-04-19 22:31:15 +0300

    Fix working with networks

--- rotten.py
+++ rotten.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import csv
-import socket
-import struct
+import ipaddress
 
 rotten_ips = {}
-domain_names = []
+networks = []
 
 
 def ip2long(ip_as_str):
-    packed = socket.inet_aton(ip_as_str)
-    ip_as_long = struct.unpack("!L", packed)[0]
-    return ip_as_long
+    return int(ipaddress.ip_address(ip_as_str))
 
 
 with open('dump.csv', 'r', errors='ignore') as csv_file:
@@ -24,23 +21,36 @@ with open('dump.csv', 'r', errors='ignore') as csv_file:
             ip = ip.strip()
             try:
                 rotten_ips[ip2long(ip)] = ip
-            except Exception:
-                print(ip)
-                domain_names.append(ip)
+            except ValueError:
+                networks.append(ipaddress.ip_network(ip))
 
 
-def check_ip(ip_str_format):
-    if ip2long(ip_str_format) in rotten_ips:
+def contains_in_single_list(ip_str):
+    return ip2long(ip_str) in rotten_ips
+
+
+def contains_in_networks_list(ip_str):
+    ip_address = ipaddress.ip_address(ip_str)
+    for network in networks:
+        if ip_address in network:
+            return True
+    return False
+
+
+def check_ip(ip_str):
+    if contains_in_single_list(ip_str) or contains_in_networks_list(ip_str):
         print('BAD NEWS')
     else:
         print('GOOD NEWS')
 
 
-print('size = %s' % (len(rotten_ips)))
+print('single ip addresses size = %s' % (len(rotten_ips)))
+print('networks size = %s' % (len(networks)))
+
+# TESTS
 # BAD NEWS
 check_ip('87.117.232.174')
 # BAD NEWS
-# BROKEN cause range not parsed correctly
 check_ip('74.82.64.1')
 # GOOD NEWS
 check_ip('192.168.99.11')
